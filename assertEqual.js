c66eb98a99315463ced4b2fa60cc29a6fd0371e2
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-01 21:19:04 +0000

    created assertEqual function which prints a message to the console if the assertion passes or fails:

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅  Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Foobar", "Foobar");
+assertEqual(1, 0);
+assertEqual('1', 1);
