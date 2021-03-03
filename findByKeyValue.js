5078c64b330c91d87095f251f0e675f69b367965
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-03 21:28:59 +0000

    findKeyByValue.js loops through an object to the key to a given value

---
+++ findByKeyValue.js
@@ -0,0 +1,26 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: + ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: + ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (keyValuePairs, targetValue) {
+  for (item in keyValuePairs){
+    if (keyValuePairs[item] === targetValue) {
+      return item;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
