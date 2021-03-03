a342839a4269ee05af16370d99ecdbb2793d0a01
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-03 21:58:48 +0000

    modified eqObjects.js to be able to handle arrays as values

--- eqObjects.js
+++ eqObjects.js
@@ -8,8 +8,51 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(firstArray, secondArray) {
+
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+
+  for (let arrayIndex = 0; arrayIndex < firstArray.length; arrayIndex++) {
+    if (firstArray[arrayIndex] !== secondArray[arrayIndex]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length){
+    return false;
+  }
+  for (const item in object1) {
+    if (Array.isArray(object1[item])){
+      if (!eqArrays(object1[item], object2[item])) {
+        return false;
+      }
+    } else {
+      if (object1[item] !== object2[item]){
+        return false;
+      }
+    }
+  }
+  return true;
 };
 
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc)); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
