81963109f7c3cc0a322ce29e62fc6b5a97ca8cd2
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-03 21:33:43 +0000

    created eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: + ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: + ${actual} !== ${expected}`);
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+};
+
