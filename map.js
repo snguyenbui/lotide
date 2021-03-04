56c69b70a3d44d37cde6baa355e14a2038e9d647
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-04 21:16:19 +0000

    map.js takes an array and applys a given function to each element of the array

---
+++ map.js
@@ -0,0 +1,40 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {  
+
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+const eqArrays = function(firstArray, secondArray) {
+
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+
+  for (let elem in firstArray) {
+    if (firstArray[elem] !== secondArray[elem]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+
+  if (eqArrays(actual, expected)) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(results1, [1, 2, 3, 4, 5]);
