ed072fcc0ec64cbb36896479e1f140559beb2105
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-04 23:53:50 +0000

    removing unnecessary comments and linting

--- tail.js
+++ tail.js
@@ -1,12 +1,7 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  /* if (actual === expected) {
-     console.log("✅  Assertion Passed: " + actual + " === " + expected);
-   } else {
-     console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
-   }*/
   if (actual === expected) {
-    console.log(`✅  Assertion Passed: ${actual} === ${expected} `);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected} `);
   } else {
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected} `);
   }
@@ -20,22 +15,13 @@ const tail = function(array) {
   return arrayTail;
 };
 
-// TEST CODE
-/*
- assertEqual("Lighthouse Labs", "Bootcamp");
- assertEqual(1, 1);
- assertEqual("Foobar", "Foobar");
- assertEqual(1, 0);
- assertEqual('1', 1);
- */
-
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
-let testCode1 = tail([1]);
+let testCode1 = tail([1,2]);
 console.log(testCode1);
-assertEqual(testCode1[0], undefined);
+assertEqual(testCode1.length, 1);
 
 let testCode2 = tail([]);
 console.log(testCode2);
