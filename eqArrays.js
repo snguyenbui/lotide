0199f86128079186063860ea8e4eaab02bd42a33
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-08 20:18:26 +0000

    refactored and added test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,5 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
- 
- 
+const assertEqual = require('./assertEqual')
+
 const eqArrays = function(firstArray, secondArray) {
 
   if (firstArray.length !== secondArray.length) {
@@ -24,6 +15,4 @@ const eqArrays = function(firstArray, secondArray) {
   return true;
 };
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
