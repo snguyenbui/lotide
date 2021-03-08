f037fa7284f2522f90bf785bc8465582f5d284ec
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-08 23:22:51 +0000

    refactored for mocha/chai

--- takeUntil.js
+++ takeUntil.js
@@ -1,27 +1,3 @@
-const eqArrays = function(firstArray, secondArray) {
-
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-
-  for (let elem in firstArray) {
-    if (firstArray[elem] !== secondArray[elem]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-
-  if (eqArrays(actual, expected)) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const takeUntil = function(array, callback) {
   const takenArray = [];
   for (let elem of array) {
@@ -34,15 +10,4 @@ const takeUntil = function(array, callback) {
   return takenArray;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+module.exports = takeUntil;
---
+++ test/takeUntilTest.js
@@ -0,0 +1,16 @@
+const assert = require('chai').assert;
+const takeUntil = require('../takeUntil');
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
+describe('#takeUntil', () => {
+  it(`returns [1, 2, 5, 7, 2] for [${data1}],  x => x < 0`, () => {
+    assert.deepEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+  });
+
+  it(`returns ['I've', 'been', 'to', 'Hollywood'] for [${data2}],  x => x === ','`, () => {
+    assert.deepEqual(takeUntil(data2, x => x === ','), ['I\'ve', 'been', 'to', 'Hollywood']);
+  });
+});
+
