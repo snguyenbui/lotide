2e3e7b00bf388691c61a9c1c9b701eaaf1d9eb54
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-08 21:48:50 +0000

    created index.js for exporting all functions

---
+++ index.js
@@ -0,0 +1,27 @@
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findByKeyValue = require('./findByKeyValue');
+const findKey = require('./findKey');
+const flatten = require('./flatten');
+const head = require('./head');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findByKeyValue: findByKeyValue,
+  findKey: findKey,
+  flatten: flatten,
+  head:   head,
+  letterPositions: letterPositions,
+  map: map,
+  middle: middle,
+  tail:   tail,
+  takeUntil: takeUntil,
+  without: without
+};
