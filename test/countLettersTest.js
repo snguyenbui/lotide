759c870712c236ccc27fec3b39c2b2f48a489519
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-08 22:01:10 +0000

    refactored for mocha/chai and linted

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 const countLetters = require('../countLetters');
 
-describe ('#countLetters', () => {
+describe('#countLetters', () => {
   it('returns { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 } for "lighthouse in the house"', () => {
     assert.deepEqual(countLetters("lighthouse in the house"), { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 });
   });
