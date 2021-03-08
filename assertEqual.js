6a437a683527edc5d6d8372a6e23fe4b203fe5a9
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-08 20:04:47 +0000

    refactoring and adding test files

--- assertEqual.js
+++ assertEqual.js
@@ -8,9 +8,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Foobar", "Foobar");
-assertEqual(1, 0);
-assertEqual('1', 1);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Foobar", "Foobar");
+assertEqual(1, 0);
+assertEqual('1', 1);
